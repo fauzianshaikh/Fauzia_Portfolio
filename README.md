42c36bc173707bccba52d0f79b30823011df2fab
FAUZIANSHAIKH <fauzianshaikh@gmail.com>
2021-12-30 14:09:59 +0530

    Fauzia Portfolio

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
-# Fauzia_Portfolio
+<h1 align="center">Hi 👋, I'm Fauzia Naushad Shaikh</h1>
+<h3 align="center">2.7+ years of experience as a Full Stack Developer with a demonstrated history of working in the software industry. Skilled in Google Cloud Platform(Kubernetes), Continuous Integration and Continuous Delivery (CI/CD), Apache Kafka, Jenkins, Git, Bitbucket, Grafana, Terraform. Having a firm hold on the Cloud technologies. Strong engineering professional with a Bachelor of Technology focused in Computer Science and Engineering from Maulana Azad National Institute of Technology.</h3>
+
+- 🔭 I’m currently working on [Terraform using Google Cloud services](https://github.com/FAUZIANSHAIKH/GCP-Terraform-Challenge-Lab.git)
+
+- 🌱 I’m currently learning **AWS, Microsoft Azure, Kubernetes, Docker**
+
+- 👯 I’m looking to collaborate on **Some innovation and creative DevOps project**
+
+- 👨‍💻 All of my projects are available at [fauziashaikh.netlify.com](fauziashaikh.netlify.com)
+
+- 💬 Ask me about **DevOps, GCP, Kubernetes, Docker, CI/CD, Jenkins**
+
+- 📫 How to reach me **fauziashaikh2601@gmail.com**
+
+- 📄 Know about my experiences [Resume](https://drive.google.com/file/d/15a-zj1AE-eeWMa6ikNQ5Mj0ArHQPczk5/view?usp=sharing)
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://linkedin.com/in/fauzianshaikh" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="fauzianshaikh" height="30" width="40" /></a>
+<a href="https://fb.com/fauzia.shaikh.940" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="fauzia.shaikh.940" height="30" width="40" /></a>
+</p>
+<br/>
+<a href="https://github.com/hashirshoaeb/home">Find Original Project Here!</a>
+<br/>
+<p align="center">
+  <h1 align="center">Fauzia's Portfolio ✨</h1>
+
+  <p align="center">
+    It is a personal static website/portfolio template hosted with GitHub Pages, built to showcase my recent projects.
+    Site URL / Demo:
+    <a href="TO BE UPLOADED">Fauzia PortFolio</a>
+    <br />
+    <br />
+    <a href="https://github.com/FAUZIANSHAIKH">About Me</a>
+  </p>
+</p>
+[![Site preview](/public/social-image.png)](https://FAUZIANSHAIKH
+.github.io/home)
+
+## Table of Contents
+
+- [Technology Stack 🛠️](#technology-stack-)
+- [Structure ⚓](#structure-)
+- [Sharing 💗](#sharing-)
+- [Prerequisites 🍪](#prerequisites-)
+- [Setup And Deployment 🔧](#setup-and-deployment-)
+- [Contributing 🙌](#contributing-)
+- [Issues](#issues)
+- [Upcoming Features / Improvements 🔗](#upcoming-features-/-improvements-)
+- [Showcase 🚀](#showcase-)
+- [Versão em Português :brazil:](#versao-em-portugues-)
+
+## Technology Stack 🛠️
+
+Dependencies defined in package.json:
+
+[Reactjs](https://reactjs.org/)
+| [Bootstrap](https://getbootstrap.com/)
+| [Typist](https://github.com/jstejada/react-typist)
+| [GitHub API](https://developer.github.com/v3/repos/)
+| [Instagram API](https://www.instagram.com/developer/embedding/)
+
+## Structure ⚓
+
+- Navigation bar (optional)
+- Body
+  - Name | Profession
+  - Contact / Follow / Find me / Facebook / LinkedIn / GitHub / Instagram / Email / CodePen
+  - Resume | About me
+- About Me
+  - Display picture (optional)
+  - About myself, my Interests, Goals and Hobbies
+  - Things I'm good at (Skills)
+  - Resume button
+- Recent Projects (using GitHub API) (optional)
+- Leadership (optional)
+  - Paragraph
+  - Carousel images
+- Skills (optional)
+  - Technical Skills
+  - Soft Skills
+- Footer
+  - Footer Note (optional)
+  - Copyrights - open source
+  - Acknowledgements(
+
+## Sharing 💗
+
+Project is open source. Feel free to make your own version. All you need to do is to fork this repository, edit [src/editable-stuff/config.js](./src/editable-stuff/config.js) and add resume. Mark star ⭐ if you like the project.
+
+## Prerequisites 🍪
+
+You should have [Node.js](https://nodejs.org/en/) and [Git](https://git-scm.com/) installed on your PC. You should also own a GitHub account.
+
+## Setup And Deployment 🔧
+
+1. To Get Started, Fork this repository to your GitHub account:
+2. Clone the forked repo from your account using:
+
+   ```bash
+     git clone https://github.com/<your-username>/home.git
+   ```
+
+3. Open in editor and edit [src/editable-stuff/config.js](./src/editable-stuff/config.js) file.
+
+4. Add your resume as <resume.pdf> in place of [src/editable-stuff/resume.pdf](./src/editable-stuff/)
+
+5. Edit [title](./public/index.html#L34) and meta [description](./public/index.html#L13) in [public/index.html](./public/index.html).
+6. Change URL in [package.json](./package.json) file:
+
+   ```json
+    "homepage": "https://<your-username>.github.io/home"
+   ```
+
+   Or for deployment at custom domain, refer [create-react-app.dev](https://create-react-app.dev/docs/deployment/#step-1-add-homepage-to-packagejson)
+
+7. After editing run the following bash commands:
+
+   ```bash
+   npm install
+   npm start
+   ```
+
+8. To deploy website, run:
+
+   ```bash
+    npm run build
+    npm run deploy
+   ```
+
+   Or for deployment at \<username>.github.io, refer [READMEdocs/custom-deployment.md](./READMEdocs/custom-deployment.md) and [pages.js](./pages.js)
+
+9. Congrats your site is up and running. To see it live, visit:
+
+   ```https
+     https://<your-username>.github.io/home
+   ```
+
+10. To change the thumbnail image:
+
+    - Navigate to the "public" folder.  
+    - There you will see "social-image.png".  
+    - Delete it.   
+    - Take a screenshot of your version and rename it "social-image.png" and place it there.  
+
+   Next time if you make changes, repeat from step 8.
+
+Facing issues? Feel free to contact at fauziashaikh2601@gmail.com
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,166 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Fauzia",
+  middleName: "",
+  lastName: "Shaikh",
+  message: " Passionate about changing the world with Cloud technologies. ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/FAUZIANSHAIKH",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/fauzianshaikh",
+    }
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/fauziashaikh.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/fauziashaikh.png"),
+  imageSize: 375,
+  message:
+    "Hi, I am Fauzia Shaikh. I have 2.7+ years of experience as a Full Stack Developer with a demonstrated history of working in the software industry. Skilled in Google Cloud Platform(Kubernetes), Continuous Integration and Continuous Delivery (CI/CD), Apache Kafka, Jenkins, Git, Bitbucket, Grafana, Terraform. Having a firm hold on the Cloud technologies. Strong engineering professional with a Bachelor of Technology focused in Computer Science and Engineering from Maulana Azad National Institute of Technology.",
+  resume: "https://drive.google.com/file/d/15a-zj1AE-eeWMa6ikNQ5Mj0ArHQPczk5/view?usp=sharing",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "FAUZIANSHAIKH", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/fauziashaikh.png"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/fauziashaikh.png"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  show: true,
+  heading: "Skills",
+  gcpSkills: [
+    { name: "GCP Compute Engine", value: 90 },
+    { name: "GCP Workloads", value: 90 },
+    { name: "GCP Namespace", value: 85 },
+    { name: "IAM", value: 90 },
+    { name: "GCP Load Balancers", value: 90 },
+    { name: "Google Container Registry", value: 80 },
+    { name: "Google Cloud Build Triggers", value: 75 },
+    { name: "GCP Deployments", value: 90 },
+    { name: "Google Cloud Endpoints", value: 75 },
+    { name: "GCP Storage Bucket", value: 85 },
+    { name: "GCP VPC", value: 75 },
+    { name: "GCP Subnet", value: 75 },
+  ],
+  devopsSkills: [
+    { name: "Google Cloud Platform", value: 80 },
+    { name: "CI/CD", value: 95 },
+    { name: "Groovy", value: 80 },
+    { name: "Jenkins", value: 90 },
+    { name: "Kubernetes", value: 75 },
+    { name: "Docker", value: 80 },
+    { name: "Spinnaker Platform", value: 90 },
+    { name: "Grafana", value: 70 },
+    { name: "Apache Kafka", value: 75 },
+    { name: "SonarQube", value: 75 },
+    { name: "Lighthouse", value: 75 },
+    { name: "Webhook", value: 75 },
+    { name: "Veracode", value: 75 },
+    { name: "Git", value: 85 },
+    { name: "Bitbucket", value: 85 },
+    { name: "Terraform", value: 75 },
+    { name: "Linux Administrator", value: 85 },
+    { name: "Shell Scripting", value: 85 },
+  ],
+  techSkills: [
+    { name: "Java", value: 80 },
+    { name: "MySQL", value: 90 },
+    { name: "JSON", value: 75 },
+    { name: "YAML", value: 85 },
+    { name: "PostgreSQL", value: 75 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Contact Me ☎️",
+  message:
+    "Discuss a project or just want to say hi? My Inbox is open for all:",
+  email: "fauziashaikh2601@gmail.com",
+};
+
+const experiences = {
+  show: true,
+  heading: "Experiences",
+  data: [
+    {
+      role: 'Associate Software Development Engineer 2',// Here Add Company Name
+      companylogo: require('../assets/img/publicissapientlogo.png'),
+      date: 'November 2021 – Present',
+    },
+    {
+      role: 'Associate Software Development Engineer 1',
+      companylogo: require('../assets/img/publicissapientlogo.png'),
+      date: 'June 2019 – October 2021',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
